71bb4961572ff1624f794b734c857f54ff37f846
sidrahiqbal <sidrah2019@namal.edu.pk>
2021-10-03 11:56:48 +0500

    added add function

---
+++ file3.py
@@ -0,0 +1,16 @@
+def divide (x,y):
+	return x / y
+
+print(12,7)
+
+def squareRoot(x):
+	return x ** 0.5
+
+x = squareRoot(10)
+print(x)
+
+ #python addition
+def add(x,y):
+	return (x+y)
+m=add(6,9)
+print(m)
