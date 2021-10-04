b542907d2d682c7ca0ccf7f4deb768e7c15e02d6
nidakhan2019 <nidakhan2019@namal.edu.pk>
2021-10-04 09:32:33 +0500

    nida commit

---
+++ cal0022.py
@@ -0,0 +1,25 @@
+def divide (x,y):
+	return x / y
+
+print(12,7)
+
+def squareRoot(x):
+	return x ** 0.5
+
+x = squareRoot(10)
+print(x)
+
+ #python addition
+def add(x,y):
+	return (x+y)
+m=add(6,9)
+print(m)
+def sub (x,y):
+    return x-y
+a=sub(8,14)
+def sub (x,y):
+    return x-y
+a=sub(8,14)
+multiply (x,y):
+    return x * y
+print (multiply (5,6))
